5d2e64da951ab7a00300cc701c1dec33f40233b2
TorhamDev <torhamam@gmail.com>
2021-09-02 23:51:35 -0700

    add help center

---
+++ help_center.py
@@ -0,0 +1,37 @@
+class help_text():
+    self_help = '''
+    To view the help commands and how to use the T-leker commands. 🔰 usage => /help 🔰'''
+    ping = '''
+    the ping command To check if the robot is online. 🔰 usage => /ping 🔰
+    '''
+    his = '''
+    Get a chat history. 🔰 usage => /his [Number For Get Histore] 🔰
+    example : /his 22
+    '''
+    info = '''
+    get full info From a users. You can also replay the user message or send the command in his pv. 🔰 usage => /info 🔰
+    '''
+    spm = '''A spammer to send a large number of messages. 🔰 usage /spm [text] [number] 🔰
+    example : /spm hi 10'''
+    delete = '''
+    Delete a large number of messages 🔰 usage => /del [number] 🔰
+    example : /del 12
+    '''
+    save_profile = '''
+    reply to the user you want to save profile. 🔰 usage => /save_profile 🔰
+    '''
+    all_help = f'''
+help : {self_help}
+        ➿〰️〰️〰️〰️〰️〰️〰️➿
+ping : {ping}
+        ➿〰️〰️〰️〰️〰️〰️〰️➿
+his : {his}
+        ➿〰️〰️〰️〰️〰️〰️〰️➿
+info : {info}
+        ➿〰️〰️〰️〰️〰️〰️〰️➿
+spm : {spm}
+        ➿〰️〰️〰️〰️〰️〰️〰️➿
+delete : {delete}
+        ➿〰️〰️〰️〰️〰️〰️〰️➿
+save_profile : {save_profile}
+    '''
